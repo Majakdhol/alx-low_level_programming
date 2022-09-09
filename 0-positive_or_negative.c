9d00935cfe82c2874bfb28f6250898abed7fe34e
Majakdhol <majakmarial1996@gmail.com>
2022-09-09 09:44:39 -0700

    Positive anything is better than negative nothing

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+/**
+ * main - print if number is positive, zero or negative
+ *
+ * Description: using the main function
+ * This program prints "number is postive, zero or positive"
+ * return: 0
+ */
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX /2;
+  /* your code goes there */
+if (n > 0)
+{
+	printf("%1 is positive\n", n);
+}
+else if (n == 0)
+{
+	printf("%1 is zero\n", n);
+}
+else if (n < 0)
+{
+	printf("%1 is negative\n", n);
+}
+return (0);
+}
+
+
