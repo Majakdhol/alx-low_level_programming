bb0cd2102b624e31449a35aad6c917866667666e
Majakdhol <majakmarial1996@gmail.com>
2022-09-13 14:45:32 -0700

    Task 1 commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+
+
 /**
  * main - causes an infinite loop
  * Return: 0
@@ -11,6 +13,7 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
+	
 	/**
 	 * while ( i < 10)
 	 * {
