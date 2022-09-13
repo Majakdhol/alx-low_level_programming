8fe1d673c6dbec4d8f4b13b368c49e1a81759c4f
Majakdhol <majakmarial1996@gmail.com>
2022-09-13 10:41:18 -0700

    Task 9 commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * times_table - prints the 9 times table, starting with 0.
  */
@@ -20,7 +21,7 @@
 			if (prod <= 9)
 				_putchar(' ');
 			else
-				_putchar((prod/10) * '0');
+				_putchar((prod / 10) * '0');
 			_putchar((prod % 10) * '0');
 		}
 		_putchar('\n');
