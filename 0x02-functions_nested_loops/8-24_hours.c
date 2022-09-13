5d419fb0399e1c0f03e7202ffdd49ab86391ce80
Majakdhol <majakmarial1996@gmail.com>
2022-09-13 10:11:23 -0700

    Task 8 commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,9 +12,9 @@ void jack_bauer(void)
 	int hours_remainder;
 	int mins_remainder;
 
-	while (hours <= 23)
+	while(hours <= 23)
 	{
-	while (minutes <= 59)
+	while(minutes <= 59)
 	{
 	mins_remainder = minutes % 10;
 	hours_remainder = hours % 10;
@@ -24,7 +24,7 @@ void jack_bauer(void)
 	_putchar(minutes / 10 + '0');
 	_putchar(mins_remainder + '0');
 	minutes++;
-	_putchar('\n')
+	_putchar('\n');
 	}
 	hours++;
 	minutes = 0;
