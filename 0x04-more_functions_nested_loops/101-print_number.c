73ab2cb4bdb192d1f0cf3d56475ab2d3efc5c298
Majakdhol <majakmarial1996@gmail.com>
2022-09-15 13:41:20 -0700

    Task 12 commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -4,10 +4,13 @@
 /**
  * print_number - Prints a number
  * @n: The number to print
+ *
+ * Return: Always 0
  */
+
 void print_number(int n)
 {
-	unsigned int num = n;
+	int num = n;
 
 	if (n < 0)
 	{
@@ -19,4 +22,6 @@ void print_number(int n)
 		print_number(num / 10);
 	}
 	putchar(num % 10 + '0');
+	
+	return (0);
 }
