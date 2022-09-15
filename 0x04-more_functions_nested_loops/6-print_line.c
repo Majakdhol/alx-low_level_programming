5910501f001367697ca74fefb243bca2bbfe51d7
Majakdhol <majakmarial1996@gmail.com>
2022-09-15 05:52:11 -0700

    Task 6 commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_line - printing line
+ * @n: integer param
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	for (x = 0; x <= n; x++)
+		_putchar('_');
+	_putchar('\n');
+}
