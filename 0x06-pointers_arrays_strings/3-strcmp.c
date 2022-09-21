51ba8635bcd860ca71d450b567c2272e0bffc7df
Majakdhol <majakmarial1996@gmail.com>
2022-09-21 09:17:46 -0700

    Task 3 commit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares pointers to two strings
+ * @s1: first pointer
+ * @s2: second pointer
+ *
+ * Return: Always success
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
