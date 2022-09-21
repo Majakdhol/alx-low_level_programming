25cf328799f673bbdea40366aa26a64e9b7360e9
Majakdhol <majakmarial1996@gmail.com>
2022-09-21 13:28:57 -0700

    Task 9 commit

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,27 +1,27 @@
 #include "main.h"
 
 /**
- * rot13 - encodes a string into rot13
- * @str: string to encode
+ * rot13 - Encodes a string using rot13. 
  *
- * Return: address of str
+ * Return: A pointer to the encoded string. 
  */
 char *rot13(char *str)
 {
-	int i, j;
-	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int index1 = 0, index2;
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L','M', 'N', 'O', 'P', 'Q', 'R','S', 'T', 'U', 'V', 'W', 'X','Y', 'Z', 'a', 'b', 'c', 'd','e', 'f', 'g', 'h', 'i', 'j','k', 'l', 'm', 'n', 'o', 'p','q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y','Z', 'A', 'B', 'C', 'D', 'E','F', 'G', 'H', 'I', 'J', 'K','L', 'M', 'n', 'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w','x', 'y', 'z', 'a', 'b', 'c','d', 'e', 'f', 'g', 'h', 'i','j', 'k', 'l', 'm'};
 	
-	for (i = 0; *(s + i); i++)
+	while (str[index1])
 	{
-		for (j = 0; j < 52; j++)
+		for (index2 = 0; index2 < 52; index2++)
 		{
-			if (a[j] == *(s + i))
+			if (str[index1] == alphabet[index2])
 			{
-				*(s + i) = b[j];
+				str[index1] = rot13key[index2];
 				break;
 			}
 		}
+		index1++;
 	}
 	return (str);
-}
+}	
