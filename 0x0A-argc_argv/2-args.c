54266abf980ed8ae0833c4fc6b59a07f57621e5f
Majakdhol <majakmarial1996@gmail.com>
2022-09-30 07:23:28 -0700

    Task 2 committed

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints name of the program
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always success
+ */
+int main(int argc, char *argv[])
+{
+	int count = 0;
+
+	if (argc > 0)
+	{
+		while (count < argc)
+		{
+			printf("%s\n", argv[count]);
+			count++;
+		}
+	}
+	return (0);
+}
