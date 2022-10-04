83b0c751fa28cb39bd96a384500f118ecc5815ac
Majakdhol <majakmarial1996@gmail.com>
2022-10-04 06:49:32 -0700

    Task 2 committed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,39 +3,28 @@
 
 /**
  * str_concat - a function that concatenates two strings
- * @s1: input 1
- * @s2: input 2
+ * @s1: first string
+ * @s2: second string
  *
  * Return: Always success
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *concat;
-	int i, ci;
+	char *concat_str;
+	int index, concat_index = 0, len = 0;
 
-	if (s1 == NULL)
+	if(s1 == NULL)
 		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	i = ci = 0;
-	while (s1[i] != '\0')
-		ci++;
-	while (s2[ci] != '\0')
-		ci++;
-	concat = malloc(sizeof(char) * (i + ci + 1));
-	if (concat == NULL)
+	if(s2 == NULL)
+		s2 == "";
+	for (index = 0; s1[index] || s2[index]; index++)
+		len++;
+	concat_str = malloc(sizeof(char) * len);
+	if (concat_str == NULL)
 		return (NULL);
-	i = ci = 0;
-	while (s1[i] != '\0')
-	{
-		concat[i] = s1[i];
-		i++;
-	}
-	while (s2[ci] != '\0')
-	{
-		concat[i] = s2[ci];
-		i++, ci++;
-	}
-	concat[i] = '\0';
-	return (concat);
+	for (index = 0; s1[index]; index++)
+		concat_str[concat_index++] = s1[index];
+	for (index = 0; s2[index]; index++)
+		concat_str[concat_index++] = s2[index];
+	return (concat_str);
 }
