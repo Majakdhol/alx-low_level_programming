301f88952619cb517f8f106558ed5e48c2efd6e0
Majakdhol <majakmarial1996@gmail.com>
2022-10-04 08:25:36 -0700

    Task 6 committed

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -57,7 +57,7 @@ char **strtow(char *str)
 
 	if (str == NULL || str[0] == '\0')
 		return (NULL);
-	words = count_words(str);
+	words = count_word(str);
 	if (words == 0)
 		return (NULL);
 	strings = malloc(sizeof(char *) * (words + 1));
@@ -72,11 +72,11 @@ char **strtow(char *str)
 		if (strings[w] == NULL)
 		{
 			for (; w >= 0; w--)
-				free(strigs[w]);
+				free(strings[w]);
 			free(strings);
 			return (NULL);
 		}
-		for (l = 0; i < letters; l++)
+		for (l = 0; l < letters; l++)
 			strings[w][l] = str[index++];
 		strings[w][l] = '\0';
 	}
